fea7dfc537d7e0a7708a2575b8ea1d364374d731
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-03-22 16:58:11 +0900

    feat: Taskを追加するコマンドの仮実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Starting Development tino-cli-ts...");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("tino")
+  .description("Issue / Task / Note を扱うローカル管理ツール")
+  .version("0.1.0");
+
+const task = program
+  .command("task")
+  .description("Task を操作する");
+
+task
+  .command("add <title>")
+  .description("Task を追加する")
+  
+  .option("-d, --description <description>", "説明")
+  .action((title, options) => {
+    console.log(`Adding task: ${title}`);
+
+    if (options.description) {
+      console.log(`Description: ${options.description}`);
+    }
+  });
+
+program.parse();
